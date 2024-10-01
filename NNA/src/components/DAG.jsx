3eab33a5223b93571e74109a7a8cf3e271704972
DelighteningPigeon <89842170+fillpattana@users.noreply.github.com>
2024-10-01 16:54:10 +0700

    replaced react-sigma with sigma.js however not working yet

--- NNA/src/components/DAG.jsx
+++ NNA/src/components/DAG.jsx
@@ -1,124 +1,56 @@
+import { useEffect, useRef, useState } from "react";
+import { MultiDirectedGraph } from "graphology";
+import { Sigma } from "sigma";
+import forceAtlas2 from "graphology-layout-forceatlas2";
 import nodeData from "../assets/nodeData.json";
 import nodeDataConv from "../assets/nodeDataWithConv.json";
-import { useEffect, useState } from "react";
-import { MultiDirectedGraph } from "graphology";
-import {
-  SigmaContainer,
-  useLoadGraph,
-  ControlsContainer,
-  FullScreenControl,
-  ZoomControl,
-  useRegisterEvents,
-  useSigma,
-} from "@react-sigma/core";
-import "@react-sigma/core/lib/react-sigma.min.css";
-import forceAtlas2 from "graphology-layout-forceatlas2";
 
 const sigmaStyle = { height: "800px", width: "800px" };
 
 // No Layout + Fully Connected Only
-export const LoadMultiDirectedGraph = () => {
-  const loadGraph = useLoadGraph();
+// export const LoadMultiDirectedGraph = (sigmaInstance) => {
+//   useEffect(() => {
+//     const graph = new MultiDirectedGraph();
 
-  useEffect(() => {
-    const graph = new MultiDirectedGraph();
+//     // Add nodes
+//     nodeData.Graph.nodes.forEach((node, index) => {
+//       graph.addNode(index.toString(), {
+//         label: `Node ${index}: ${node.type}`,
+//         size: 15 + node.weights[0].length, // Adjust size based on number of weights
+//         x: Math.random(),
+//         y: Math.random(),
+//         color: "#FA4F40",
+//       });
+//     });
 
-    // Add nodes
-    nodeData.Graph.nodes.forEach((node, index) => {
-      console.log(`Adding node ${index}`);
-      graph.addNode(index.toString(), {
-        label: `Node ${index}: ${node.type}`,
-        size: 15 + node.weights[0].length, // Adjust size based on number of weights
-        x: Math.random(),
-        y: Math.random(),
-        color: "#FA4F40",
-      });
-    });
+//     // Add edges
+//     nodeData.Graph.edges.senders.forEach((sender, idx) => {
+//       const receiver = nodeData.Graph.edges.receivers[idx];
+//       if (
+//         graph.hasNode(sender.toString()) &&
+//         graph.hasNode(receiver.toString())
+//       ) {
+//         graph.addEdgeWithKey(
+//           `edge${idx}`,
+//           sender.toString(),
+//           receiver.toString(),
+//           {
+//             label: `Edge ${sender}-${receiver}`,
+//             color: "#4FA4F0",
+//           }
+//         );
+//       }
+//     });
 
-    // Log and check before adding edges
-    nodeData.Graph.edges.senders.forEach((sender, idx) => {
-      const receiver = nodeData.Graph.edges.receivers[idx];
-      console.log(`Adding edge from ${sender} to ${receiver}`);
-      console.log(`Edge Number: ${idx}`);
-      if (
-        graph.hasNode(sender.toString()) &&
-        graph.hasNode(receiver.toString())
-      ) {
-        graph.addEdgeWithKey(
-          `edge${idx}`,
-          sender.toString(),
-          receiver.toString(),
-          {
-            label: `Edge ${sender}-${receiver}`,
-            color: "#4FA4F0",
-          }
-        );
-      } else {
-        console.error(`Node ${sender} or ${receiver} not found`);
-      }
-    });
+//     // Load the graph into the sigma instance
+//     sigmaInstance.setGraph(graph);
+//   }, [sigmaInstance]);
 
-    loadGraph(graph);
-  }, [loadGraph]);
-
-  return null;
-};
-//
-//Library Node Auto Layout Without Conv
-export const LoadLibLayoutGraph = () => {
-  const loadGraph = useLoadGraph();
-
-  useEffect(() => {
-    const graph = new MultiDirectedGraph();
-
-    // Add nodes
-    nodeDataConv.Graph.nodes.forEach((node, index) => {
-      graph.addNode(index.toString(), {
-        label: `Node ${index}: ${node.type}`,
-        size: 15 + node.params.weights[0].length,
-        x: Math.random(), // Initial random positions
-        y: Math.random(),
-        color: "#FA4F40",
-        adjustSizes: true,
-      });
-    });
-
-    // Add edges
-    nodeData.Graph.edges.senders.forEach((sender, idx) => {
-      const receiver = nodeData.Graph.edges.receivers[idx];
-      if (
-        graph.hasNode(sender.toString()) &&
-        graph.hasNode(receiver.toString())
-      ) {
-        graph.addEdgeWithKey(
-          `edge${idx}`,
-          sender.toString(),
-          receiver.toString(),
-          {
-            label: `Edge ${sender}-${receiver}`,
-            color: "#4FA4F0",
-            size: 3,
-          }
-        );
-      }
-    });
-
-    // Apply ForceAtlas2 layout
-    forceAtlas2.assign(graph, {
-      iterations: 50,
-      settings: { gravity: 1, scalingRatio: 2 },
-    });
-
-    loadGraph(graph);
-  }, [loadGraph]);
-
-  return null;
-};
+//   return null;
+// };
 
 // Library Node Auto Layout With Conv
-export const LoadLibLayoutGraph2 = () => {
-  const loadGraph = useLoadGraph();
-
+export const LoadLibLayoutGraph2 = (sigmaInstance) => {
   useEffect(() => {
     const graph = new MultiDirectedGraph();
 
@@ -126,20 +58,17 @@ export const LoadLibLayoutGraph2 = () => {
     nodeDataConv.Graph.nodes.forEach((node, index) => {
       let color = "#68696c"; // Default color
 
-      // Adjust color based on the type of the layer
       if (node.type === "fully connected") {
         color = "#4065fa";
-        // size = node.params.weights.flat().length + 10;
       } else if (node.type === "convolutional") {
         color = "#FA4F40";
-        // size = node.params.weights.flat().length + 10;
       }
 
       // Add the node to the graph
       graph.addNode(index.toString(), {
         label: `Node ${index}: ${node.type}`,
         size: node.params.weights.flat().length + 10,
-        x: Math.random(), // Initial random positions
+        x: Math.random(),
         y: Math.random(),
         color: color,
         adjustSizes: true,
@@ -161,7 +90,6 @@ export const LoadLibLayoutGraph2 = () => {
             label: `Edge ${sender}-${receiver}`,
             color: "#4FA4F0",
             size: 3,
-            defaultEdgeType: "curve",
           }
         );
       }
@@ -173,73 +101,84 @@ export const LoadLibLayoutGraph2 = () => {
       settings: { gravity: 1, scalingRatio: 2 },
     });
 
-    loadGraph(graph);
-  }, [loadGraph]);
+    // Load the graph into the sigma instance
+    sigmaInstance.setGraph(graph);
+  }, [sigmaInstance]);
 
   return null;
 };
 
-export const DragandDrop = () => {
-  const registerEvents = useRegisterEvents();
-  const sigma = useSigma();
+// Drag and Drop
+export const DragandDrop = (sigmaInstance) => {
   const [draggedNode, setDraggedNode] = useState(null);
 
   useEffect(() => {
-    // Register the events
-    registerEvents({
-      downNode: (e) => {
-        setDraggedNode(e.node);
-        sigma.getGraph().setNodeAttribute(e.node, "highlighted", true);
-      },
-      // On mouse move, if the drag mode is enabled, we change the position of the draggedNode
-      mousemovebody: (e) => {
-        if (!draggedNode) return;
-        // Get new position of node
-        const pos = sigma.viewportToGraph(e);
-        sigma.getGraph().setNodeAttribute(draggedNode, "x", pos.x);
-        sigma.getGraph().setNodeAttribute(draggedNode, "y", pos.y);
+    const graph = sigmaInstance.getGraph();
 
-        // Prevent sigma to move camera:
-        e.preventSigmaDefault();
-        e.original.preventDefault();
-        e.original.stopPropagation();
-      },
-      // On mouse up, we reset the autoscale and the dragging mode
-      mouseup: () => {
-        if (draggedNode) {
-          setDraggedNode(null);
-          sigma.getGraph().removeNodeAttribute(draggedNode, "highlighted");
-        }
-      },
-      // Disable the autoscale at the first down interaction
-      mousedown: () => {
-        if (!sigma.getCustomBBox()) sigma.setCustomBBox(sigma.getBBox());
-      },
-    });
-  }, [registerEvents, sigma, draggedNode]);
+    const handleDownNode = (event) => {
+      setDraggedNode(event.node);
+      graph.setNodeAttribute(event.node, "highlighted", true);
+    };
+
+    const handleMousemove = (event) => {
+      if (!draggedNode) return;
+
+      const pos = sigmaInstance.viewportToGraph(event);
+      graph.setNodeAttribute(draggedNode, "x", pos.x);
+      graph.setNodeAttribute(draggedNode, "y", pos.y);
+
+      event.preventDefault();
+    };
+
+    const handleMouseup = () => {
+      if (draggedNode) {
+        graph.removeNodeAttribute(draggedNode, "highlighted");
+        setDraggedNode(null);
+      }
+    };
+
+    sigmaInstance.on("downNode", handleDownNode);
+    window.addEventListener("mousemove", handleMousemove);
+    window.addEventListener("mouseup", handleMouseup);
+
+    return () => {
+      sigmaInstance.removeListener("downNode", handleDownNode);
+      window.removeEventListener("mousemove", handleMousemove);
+      window.removeEventListener("mouseup", handleMouseup);
+    };
+  }, [sigmaInstance, draggedNode]);
 
   return null;
 };
 
+// Main DAG Component
 function DAG() {
+  const containerRef = useRef(null);
+  const sigmaInstanceRef = useRef(null);
+
+  useEffect(() => {
+    if (containerRef.current) {
+      const graph = new MultiDirectedGraph();
+      sigmaInstanceRef.current = new Sigma(graph, containerRef.current);
+
+      // Clean up when component unmounts
+      return () => {
+        sigmaInstanceRef.current.kill();
+      };
+    }
+  }, []);
+
+  const sigmaInstance = sigmaInstanceRef.current;
+
   return (
-    <SigmaContainer
-      graph={MultiDirectedGraph}
-      style={sigmaStyle}
-      settings={{
-        allowInvalidContainer: true,
-      }}
-    >
-      {/* <LoadNormalGraph /> */}
-      {/* <LoadMultiDirectedGraph /> */}
-      {/* <LoadLibLayoutGraph /> */}
-      <LoadLibLayoutGraph2 />
-      <DragandDrop />
-      <ControlsContainer position={"bottom-right"}>
-        <ZoomControl />
-        <FullScreenControl />
-      </ControlsContainer>
-    </SigmaContainer>
+    <div ref={containerRef} style={sigmaStyle}>
+      {sigmaInstance && (
+        <>
+          <LoadLibLayoutGraph2 sigmaInstance={sigmaInstance} />
+          <DragandDrop sigmaInstance={sigmaInstance} />
+        </>
+      )}
+    </div>
   );
 }
 
