2ad9238800f63fe8597adc7b194b574408840195
DelighteningPigeon <89842170+fillpattana@users.noreply.github.com>
2024-10-28 12:26:48 +0700

    DAG NavBar + Properties DropDown
    Setup for Agent Groups selection
    Properties drop down and may be irritating for users, optionally could be displayed to the right side of the DAG canvas

---
+++ NNA/src/components/dagComps/DAGNavBar.jsx
@@ -0,0 +1,45 @@
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
+import Properties from "../Properties";
+
+function DAGNavBar({ node }) {
+  return (
+    <Navbar expand="lg" className="bg-body-tertiary">
+      <Container fluid>
+        <Navbar.Toggle aria-controls="navbarScroll" />
+        <Navbar.Collapse id="navbarScrollable">
+          <Nav
+            className="me-auto my-2 my-lg-0"
+            style={{ maxHeight: "100px" }}
+            navbarScroll
+          >
+            <Nav.Link href="#Agent1">Agent1</Nav.Link>
+            <Nav.Link href="#Agent2">Agent2</Nav.Link>
+            <Nav.Link href="#Agent3">Agent3</Nav.Link>
+            <Nav.Link href="#Agent4">Agent4</Nav.Link>
+            <Nav.Link href="#Agent5">Agent5</Nav.Link>
+            <Nav.Link href="#Agent6">Agent6</Nav.Link>
+            <Nav.Link href="#Agent7">Agent7</Nav.Link>
+            <Nav.Link href="#Agent8">Agent8</Nav.Link>
+          </Nav>
+          <NavDropdown
+            title="Properties"
+            autoClose="inside"
+            drop="down"
+            align={{ lg: "end" }}
+            id="diagram-properties"
+          >
+            <NavDropdown.Header as="div">
+              {" "}
+              <Properties node={node} />{" "}
+            </NavDropdown.Header>
+          </NavDropdown>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default DAGNavBar;
