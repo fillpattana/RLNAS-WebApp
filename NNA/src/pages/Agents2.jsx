9ee9ef8c7ea4659fc29599c3d5385909180678dc
DelighteningPigeon <89842170+fillpattana@users.noreply.github.com>
2024-11-03 11:53:30 +0700

    topics distinction stylings

--- NNA/src/pages/Agents2.jsx
+++ NNA/src/pages/Agents2.jsx
@@ -28,68 +28,66 @@ function Agents() {
   }, []);
 
   return (
-    <Container>
-      <Row>
-        <h3> Network Architecture - Direct Acyclic Graph </h3>
-        <Col>
-          <AgentGroups />
-        </Col>
-        <Col>
-          {/* <div className="centered-container">
+    <div>
+      <Container className="agents-background">
+        <Row>
+          <h3 className="topic-name">
+            Network Architecture - Direct Acyclic Graph
+          </h3>
+          <Col>
+            <AgentGroups />
+          </Col>
+          <Col>
+            {/* <div className="centered-container">
             <div className="dag-container">
               <DAGNavBar node={selectedNode} />
               <DAGSugi onNodeClick={handleNodeClick} />
             </div>
           </div> */}
-          <div className="centered-container">
-            <Carousel
-              fade
-              interval={null}
-              activeIndex={index}
-              onSelect={handleNextIteration}
-              data-bs-theme="dark"
-            >
-              <Carousel.Item>
-                <div className="dag-container">
-                  <DAGNavBar node={selectedNode} />
-                  <DAGSugi onNodeClick={handleNodeClick} />
-                </div>
-                <Carousel.Caption>
-                  <h5>Agent Group 1</h5>
-                  <p>Episode 1 - Iteration 1</p>
-                </Carousel.Caption>
-              </Carousel.Item>
-              <Carousel.Item>
-                <div className="dag-container">
-                  <DAGNavBar node={selectedNode} />
-                  <DAGSugi onNodeClick={handleNodeClick} />
-                </div>
-                <Carousel.Caption>
-                  <h5>Agent Group 1</h5>
-                  <p>Episode 1 - Iteration 2</p>
-                </Carousel.Caption>
-              </Carousel.Item>
-            </Carousel>
-          </div>
-        </Col>
-      </Row>
-      <Row>
-        <Col>
-          <h3> Biaxial Chart - Training Time and Accuracy </h3>
-          <div className="centered-container">
-            <BiaxialChart />
-          </div>
-        </Col>
-      </Row>
-      <Row>
-        <Col>
-          <h3> Single Line Chart - Loss/Epoch Chart </h3>
-          <div className="centered-container">
-            <SingleLineChart />
-          </div>
-        </Col>
-      </Row>
-    </Container>
+            <div className="centered-container">
+              <Carousel
+                fade
+                interval={null}
+                activeIndex={index}
+                onSelect={handleNextIteration}
+                data-bs-theme="dark"
+              >
+                <Carousel.Item>
+                  <div className="dag-container">
+                    <DAGNavBar node={selectedNode} />
+                    <DAGSugi onNodeClick={handleNodeClick} />
+                  </div>
+                  <Carousel.Caption>
+                    <h5>Agent Group 1</h5>
+                    <p>Episode 1 - Iteration 1</p>
+                  </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                  <div className="dag-container">
+                    <DAGNavBar node={selectedNode} />
+                    <DAGSugi onNodeClick={handleNodeClick} />
+                  </div>
+                  <Carousel.Caption>
+                    <h5>Agent Group 1</h5>
+                    <p>Episode 1 - Iteration 2</p>
+                  </Carousel.Caption>
+                </Carousel.Item>
+              </Carousel>
+            </div>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <h3 className="topic-name">
+              Biaxial Chart - Training Time and Accuracy
+            </h3>
+            <div className="centered-container">
+              <BiaxialChart />
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </div>
   );
 }
 
--- NNA/src/pages/Overview.jsx
+++ NNA/src/pages/Overview.jsx
@@ -4,9 +4,9 @@ import BiaxialLineChart from "../components/BiaxialChart";
 function Overview() {
   return (
     <div>
-      <h3> Epoch/Loss of Overall Agents </h3>
+      <h3> Overall Epoch/Loss of Agents </h3>
       <SingleLineChart />
-      <h3> Training Time and Accuracy of Overall Agents </h3>
+      <h3> Overall Training Time and Accuracy of Agents </h3>
       <BiaxialLineChart />
     </div>
   );
