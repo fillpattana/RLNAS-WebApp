74dc83b55936c1088212aff34619988528d42f92
DelighteningPigeon <89842170+fillpattana@users.noreply.github.com>
2025-03-26 20:09:24 +0700

    Overview Charts working but not realtime

--- NNA/src/pages/Overview.jsx
+++ NNA/src/pages/Overview.jsx
@@ -1,5 +1,5 @@
 import LossChart from "../components/LossChart";
-import OverallPerformanceChart from "../components/OverallPerformanceChart";
+import OverallAccuracyChart from "../components/OverallAccuracyChart";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
@@ -18,7 +18,7 @@ function Overview() {
           <div className="elements-container">
             <Col>
               <h3> Overall Training Time and Accuracy of All Agents </h3>
-              <OverallPerformanceChart />
+              <OverallAccuracyChart />
             </Col>
           </div>
         </Row>
